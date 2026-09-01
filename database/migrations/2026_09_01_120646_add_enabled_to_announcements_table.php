ae900a6fa8b0f31e4ccfb743ea4f33c6ed8cfd59
haidar1306 <haidarmaknojiya1306@gmail.com>
2026-09-01 17:37:55 +0530

    save

---
+++ database/migrations/2026_09_01_120646_add_enabled_to_announcements_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEnabledToAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+   public function up(): void
+{
+    Schema::table('announcements', function (Blueprint $table) {
+        $table->boolean('enabled')->default(true);
+    });
+}
+
+public function down(): void
+{
+    Schema::table('announcements', function (Blueprint $table) {
+        $table->dropColumn('enabled');
+    });
+}
+}
