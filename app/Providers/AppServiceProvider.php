f9b15b97926e7dbdf513e9d4893741088834aff3
haidar1306 <haidarmaknojiya1306@gmail.com>
2026-09-02 12:55:02 +0530

    fix: force https scheme for all generated urls

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,31 +2,20 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
-/**
- * Class AppServiceProvider.
- */
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-   public function boot()
-{
-    Paginator::useBootstrap();
-}
+    public function boot()
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
 }
