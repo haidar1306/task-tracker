b4fd064227d71064ac65baecbfd0ce3e0486ccaf
haidar1306 <haidarmaknojiya1306@gmail.com>
2026-09-11 10:34:17 +0530

    Disable two factor authentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,10 +51,6 @@ class Kernel extends HttpKernel
         ],
 
         'admin' => [
-            /*
-             * This is configurable, disable boilerplate.access.user.admin_requires_2fa instead of removing this
-             */
-            '2fa:enabled',
             'auth',
             'password.expires',
             'is_admin',
