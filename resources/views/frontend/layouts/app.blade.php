7f4a86a7cde4627ddfdf4baf914e8ea1f7f136f5
haidar1306 <haidarmaknojiya1306@gmail.com>
2026-09-16 11:13:29 +0530

    save

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -140,8 +140,7 @@
     @stack('after-styles')
 </head>
 
-<body class="{{ request()->routeIs('frontend.index', 'frontend.room.index', 'frontend.amenities.index', 'frontend.about', 'frontend.reservation.index', 'frontend.contact', 'frontend.services.index') ? 'home-page' : '' }}">    @include('includes.partials.read-only')
-    @include('includes.partials.logged-in-as')
+<body class="{{ request()->routeIs('frontend.index', 'frontend.user.dashboard', 'frontend.room.index', 'frontend.amenities.index', 'frontend.about', 'frontend.reservation.index', 'frontend.contact') ? 'home-page' : '' }}">    @include('includes.partials.logged-in-as')
 
     <div id="app">
 
